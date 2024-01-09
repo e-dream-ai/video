fcb29b0a0072755eb82d95b13ad9b08edc4cbb79
dealparadize <ealejandrocasillas@gmail.com>
2024-01-08 21:48:47 -0700

    feat(video): process video handler

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import os
-import ffmpeg
-from s3 import download_file, upload_file
-from convert_video import convert_video, generate_thumbnail
-from flask import Flask
+import threading
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
-from flask import jsonify
+from process_video import process_video
+
 
 load_dotenv()
 
@@ -14,32 +13,13 @@ env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
 
 
-@app.route("/convert_video")
-def index():
-    dream_uuid = "177d577d-99f8-4125-9584-0656eb6b7f5f"
-    user_uuid = "0cfcfb03-cab5-490a-b7cf-3ab8bfd4f51c"
-    is_dream_downloaded = download_file(
-        file_name="./assets/{}.mp4".format(dream_uuid),
-        object_name="{}/{}/{}.mp4".format(user_uuid, dream_uuid, dream_uuid),
-    )
+@app.route("/process_video", methods=["POST"])
+def process_video_handler():
+    content = request.json
+    user_uuid = content["user_uuid"]
+    dream_uuid = content["dream_uuid"]
 
-    if not is_dream_downloaded:
-        return jsonify(sucess=False, status=404, message="Dream not found")
-    convert_video(
-        input_file="./assets/{}.mp4".format(dream_uuid),
-        output_file="./assets/{}_output.mp4".format(dream_uuid),
-    )
-    generate_thumbnail(
-        input_file="./assets/{}.mp4".format(dream_uuid),
-        output_file="./assets/{}.png".format(dream_uuid),
-    )
-    upload_file(
-        file_name="./assets/{}.mp4".format(dream_uuid),
-        object_name="{}/{}/{}_converted.mp4".format(user_uuid, dream_uuid, dream_uuid),
-    )
-    upload_file(
-        file_name="./assets/{}.png".format(dream_uuid),
-        object_name="{}/{}/{}.png".format(user_uuid, dream_uuid, dream_uuid),
-    )
+    thread = threading.Thread(target=process_video, args=(user_uuid, dream_uuid))
+    thread.start()
 
     return jsonify(sucess=True, status=200, message="Video successfully converted")
---
+++ process_video.py
@@ -0,0 +1,31 @@
+import os
+from s3 import download_file, upload_file
+from convert_video import convert_video, generate_thumbnail
+
+
+def process_video(user_uuid, dream_uuid):
+    download_file(
+        file_name="./assets/{}.mp4".format(dream_uuid),
+        object_name="{}/{}/{}.mp4".format(user_uuid, dream_uuid, dream_uuid),
+    )
+
+    convert_video(
+        input_file="./assets/{}.mp4".format(dream_uuid),
+        output_file="./assets/{}_converted.mp4".format(dream_uuid),
+    )
+    generate_thumbnail(
+        input_file="./assets/{}.mp4".format(dream_uuid),
+        output_file="./assets/{}.png".format(dream_uuid),
+    )
+    upload_file(
+        file_name="./assets/{}.mp4".format(dream_uuid),
+        object_name="{}/{}/{}_converted.mp4".format(user_uuid, dream_uuid, dream_uuid),
+    )
+    upload_file(
+        file_name="./assets/{}.png".format(dream_uuid),
+        object_name="{}/{}/thumbnails/{}.png".format(user_uuid, dream_uuid, dream_uuid),
+    )
+
+    os.remove("./assets/{}.mp4".format(dream_uuid))
+    os.remove("./assets/{}_converted.mp4".format(dream_uuid))
+    os.remove("./assets/{}.png".format(dream_uuid))
